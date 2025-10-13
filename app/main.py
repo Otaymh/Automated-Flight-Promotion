4d210d298cd38e6589118223bde8924ce33ef76b
Otaymh <otaymhabutaha7@gmail.com>
2025-10-13 13:20:05 +0300

    Update main.py with new image processing logic

--- app/main.py
+++ app/main.py
@@ -38,8 +38,8 @@ app.add_middleware(
 
 app.state.limiter = limiter
 
-# Database setup it's SQLite
-DATABASE_URL = "sqlite:///./flight_posts.db"
+# Database it's supabase
+DATABASE_URL = "DATABASE_URLL"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(bind=engine)
